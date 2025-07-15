36e7ab715b69adb582731b3089e998381cb61051
Medha jha <145251277+medhajha810@users.noreply.github.com>
2025-07-15 11:36:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int longestCommonSubstr(string& s1, string& s2) {
+        // your code here
+        int ans=0;
+        int m=s1.size();
+        int n=s2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=0;
+                }
+                ans=max(ans,dp[i][j]);
+            }
+        }
+        return ans;
+    }
+};
